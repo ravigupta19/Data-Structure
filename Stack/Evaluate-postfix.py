47dbd15b5e636556a343cc19c9daf45df8f100f3
Ravi Gupta <ravirocks325@gmail.com>
2017-04-13 13:43:55 +0530

    Evaluate postfix expression  using Stack

---
+++ Stack/Evaluate-postfix.py
@@ -0,0 +1,46 @@
+class Evaluate(object):
+
+    def __init__(self):
+        self.stk = []
+        self.top = None
+
+    def pop(self):
+        try:
+            res = self.stk.pop()
+            self.top = self.stk[-1]
+        except IndexError:
+            self.top = None
+        return res
+
+    def push(self,data):
+        self.stk.append(data)
+        self.top = self.stk[-1]
+
+    def evaluatePostfix(self, expression):
+        for char in expression:
+            try:
+                data = int(char)
+            except ValueError:
+                oper2 = self.pop()
+                oper1 = self.pop()
+                res = self.evaluateExpression(char,oper1, oper2)
+                self.push(res)
+            else:
+                self.push(data)
+        print(self.getTop())
+
+    def evaluateExpression(self,operand, operator1, operator2):
+        if operand == '*':
+            return operator1 * operator2
+        elif operand == '/':
+            return operator1 // operator2
+        elif operand == '+':
+            return operator1 + operator2
+        elif operand == '-':
+            return operator1 - operator2
+
+    def getTop(self):
+        return self.top
+
+evalexper = Evaluate()
+evalexper.evaluatePostfix('23*54*+9-')
