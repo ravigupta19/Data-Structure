7a480aad02d2f2f40afd360060f2280b246fa16d
Ravi Gupta <ravirocks325@gmail.com>
2017-04-24 16:37:47 +0530

    Complete Binary Search Tree with Travesal

--- Trees/Binary-Tree.py
+++ Trees/Binary-Tree.py
@@ -67,6 +67,48 @@ class BinaryTree(object):
         else:
             return self.getMax(root.right)
 
+    def FindHeight(self, root):
+        if root is None:
+            return -1
+        return max(self.FindHeight(root.left), self.FindHeight(root.right)) + 1
+
+    def leveorder(self):
+        if self.root is None:
+            return
+        else:
+            l = []
+            l.append(self.root)
+            while len(l) != 0:
+                node = l.pop(0)
+                print(node.data)
+                if node.left is not None:
+                    l.append(node.left)
+                if node.right is not None:
+                    l.append(node.right)
+
+    def preorder(self,root):
+        if root is None:
+            return
+        print(root.data)
+        self.preorder(root.left)
+        self.preorder(root.right)
+
+    def inorder(self, root):
+        if root is None:
+            return
+        self.inorder(root.left)
+        print(root.data)
+        self.inorder(root.right)
+
+    def postorder(self, root):
+        if root is None:
+            return
+        self.postorder(root.left)
+        self.postorder(root.right)
+        print(root.data)
+
+
+
 BT  = BinaryTree()
 node = BT.getNewNode(15)
 BT.InsertNode(BT.root,node)
@@ -84,6 +126,11 @@ node = BT.getNewNode(28)
 BT.InsertNode(BT.root,node)
 node = BT.getNewNode(45)
 BT.InsertNode(BT.root,node)
-print(BT.getMin(BT.root))
-print(BT.getMax(BT.root))
-print(BT.SearchNode(BT.root,45))
+#print(BT.getMin(BT.root))
+#print(BT.getMax(BT.root))
+#print(BT.SearchNode(BT.root,45))
+#print(BT.FindHeight(BT.root))
+#BT.leveorder()
+BT.preorder(BT.root)
+BT.inorder(BT.root)
+BT.postorder(BT.root)
